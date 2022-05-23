0ca9dbe1d9ce67370e65c2368b812839a111252c
Ignacio Yz <ignacioyz@gmail.com>
2022-05-23 08:54:43 -0400

    commit tree 73441845bb3a611352e92209ef428645daabfc8c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const delMain = document.getElementById('main')
+delMain.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = 'Ignacio is the champion'
